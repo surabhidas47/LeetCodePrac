eb83d304696bef67da608186c4c373b7f688b86d
surabhi <surabhi.das@outlook.com>
2023-09-25 13:09:58 -0400

    2. Add Two Numbers

--- WhiteBoardPrac/src/AddTwoNums.java
+++ WhiteBoardPrac/src/AddTwoNums.java
@@ -1,5 +1,5 @@
 public class AddTwoNums {
-
+//1. Two Sum
 //    Given an array of integers nums and an integer target, return
 //    indices of the two numbers such that they add up to target.
 //    Input: nums = [2,7,11,15], target = 9
