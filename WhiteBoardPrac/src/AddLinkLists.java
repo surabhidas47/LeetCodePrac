fda74a284b96366d63629a08be9a94b01bbd2514
surabhi <surabhi.das@outlook.com>
2023-09-26 18:37:20 -0400

    3 Longest substring wo repeated ch

--- WhiteBoardPrac/src/AddLinkLists.java
+++ WhiteBoardPrac/src/AddLinkLists.java
@@ -13,40 +13,40 @@ public class AddLinkLists {
     //2. Add two Numbers
     //You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
     // and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
-    class Solution {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            ListNode result = new ListNode (0);
-            ListNode temp = result;
-            int carry = 0;
-
-            while(l1!=null||l2!=null){
-                int l1Val = (l1 != null) ? l1.val: 0;
-                int l2Val = (l2 != null) ? l2.val: 0;
-
-                int currentSum = l1Val + l2Val + carry;
-                // 10/10 = 1
-                carry = currentSum/10;
-                //10%10 = 0
-                int lastDigit = currentSum%10;
-
-                ListNode newNode = new ListNode(lastDigit);
-                temp.next = newNode;
-
-                if (l1 != null) l1= l1.next;
-                if (l2 != null) l2 = l2.next;
-                temp = temp.next;
-            }
-
-            if (carry>0){
-                ListNode newNode = new ListNode (carry);
-                temp.next = newNode;
-                temp = temp.next;
-            }
-            return result.next;
-        }
-    }
-
-
-
-
+//    class Solution {
+//        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//            ListNode result = new ListNode (0);
+//            ListNode temp = result;
+//            int carry = 0;
+//
+//            while(l1!=null||l2!=null){
+//                int l1Val = (l1 != null) ? l1.val: 0;
+//                int l2Val = (l2 != null) ? l2.val: 0;
+//
+//                int currentSum = l1Val + l2Val + carry;
+//                // 10/10 = 1
+//                carry = currentSum/10;
+//                //10%10 = 0
+//                int lastDigit = currentSum%10;
+//
+//                ListNode newNode = new ListNode(lastDigit);
+//                temp.next = newNode;
+//
+//                if (l1 != null) l1= l1.next;
+//                if (l2 != null) l2 = l2.next;
+//                temp = temp.next;
+//            }
+//
+//            if (carry>0){
+//                ListNode newNode = new ListNode (carry);
+//                temp.next = newNode;
+//                temp = temp.next;
+//            }
+//            return result.next;
+//        }
+//    }
+//
+//
+//
+//
 }
---
+++ WhiteBoardPrac/src/threeLongestSubstr.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class threeLongestSubstr {
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+
+            int maxLength =0;
+
+            Map<Character, Integer> checkChar = new HashMap<>();
+
+            //incrementing left pointer when we come across duplicate
+            //incrementing right at every iteration
+            for (int right =0, left =0; right <s.length();right++){
+                //want to check if map already contains and if its within the
+                //substring we are currently dealing with
+                char currentCharacter = s.charAt(right);
+                if(checkChar.containsKey(currentCharacter) && checkChar.get(currentCharacter)>=left) {
+                    //if all of that is true^^... character is in map and position of the characater
+                    //is greater than/ equal to the left pointer, then we move the left pointer over one
+                    left = checkChar.get(currentCharacter) + 1;
+                }
+                //+1 bc what if substring is just first letter, 0+0
+                maxLength = Math.max(maxLength, right-left+1);
+                //else
+                checkChar.put(currentCharacter,right);
+
+            }
+            return maxLength;
+        }
+    }
+}
